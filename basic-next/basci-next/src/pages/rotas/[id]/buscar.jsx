388c5213608a95f73e7a5d530f682c1c77c06ba5
victoradauto1 <46942765+victoradauto1@users.noreply.github.com>
2023-04-03 16:12:05 -0300

    rotas e rotas dinâmicas

---
+++ basic-next/basci-next/src/pages/rotas/[id]/buscar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function  rotas() {
+  return (
+    <div> 
+        <h1>rotas / id /buscar</h1></div>
+  )
+}
---
+++ basic-next/basci-next/src/pages/rotas/_OutraRota.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function _OutraRota() {
+  return (
+    <div>
+        Outra Rota dentro da pasta
+    </div>
+  )
+}
