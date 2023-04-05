046383696bd3a1b5cfd9977bc61d220e2731363f
victoradauto1 <46942765+victoradauto1@users.noreply.github.com>
2023-04-05 15:29:37 -0300

    rotas dinâmicas

--- basic-next/basci-next/src/pages/rotas/index.jsx
+++ basic-next/basci-next/src/pages/rotas/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
   return (
     <div>
         <h1>Rota para index</h1>
+        <ul>
+          <Link href="/rotas/params?id=12&nome=Ana">
+          <li>Params</li>
+          </Link>
+        </ul>
     </div>
   )
 }
