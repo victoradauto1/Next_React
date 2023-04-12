9c711e15d850aab87b0840fd9d258e0ffa959a98
Victor Adauto <victoradauto@yahoo.com.br>
2023-04-11 22:27:31 -0300

    finalização de conceitos inciais da estrutura do next

---
+++ entendendo/entendendo/src/pages/contato.tsx
@@ -0,0 +1,7 @@
+export default function Contato () {
+    return (
+        <div>
+            <h1>Página de Contato</h1>
+        </div>
+    )
+}
---
+++ entendendo/entendendo/src/pages/sobre.tsx
@@ -0,0 +1,9 @@
+
+
+export default function sobre() {
+  return (
+    <div>
+        <h1>Página Sobre</h1>
+    </div>
+  )
+}
