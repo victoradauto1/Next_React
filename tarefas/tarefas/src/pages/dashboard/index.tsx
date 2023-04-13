5777b5db6d4a37444ad4271ecff362b5abb23f8e
Victor Adauto <victoradauto@yahoo.com.br>
2023-04-13 16:43:43 -0300

    finalização da view da home, criação de header e instalação da next auth

---
+++ tarefas/tarefas/src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+import Head from 'next/head'
+
+export default function Dashboad (){
+    return(
+        <div className={styles.dashboard}>
+            <Head>
+                <h1>Painel de Tarefas</h1>
+            </Head>
+            
+            <h1>Página Painel</h1>
+
+        </div>
+    )
+}
