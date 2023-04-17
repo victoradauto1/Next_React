b5bf9ac77cc56ec8b39f03746e449e9073e3f127
Victor Adauto <victoradauto@yahoo.com.br>
2023-04-17 17:00:15 -0300

    implantação do login co provider google

--- tarefas/tarefas/src/components/header/index.tsx
+++ tarefas/tarefas/src/components/header/index.tsx
@@ -1,27 +1,44 @@
-import {useSession, signIn, signOut} from 'next-auth/react'
-import styles from './styles.module.css'
-import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "./styles.module.css";
+import Link from "next/link";
 
-export function Header(){
+export function Header() {
+  const { data: session, status } = useSession();
 
-    const {data:session, status} = useSession();
-
-    return(
-        <header className={styles.header}>
-            <section className={styles.content}>
-                <nav className={styles.nav}>
-                    <Link href="/">
-                    <h1 className={styles.logo}>Tarefas<span>+</span></h1>
-                    </Link>
-                    <Link href="/dashboard" className={styles.link}>
-                    Meu Painel
-                    </Link>
-                </nav>
-                { status === "loading"? ():() }
-                <button className={styles.loginButton}>acessar</button>
-            </section>      
-        </header>
-
-        
-    )
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas<span>+</span>
+            </h1>
+          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.link}>
+              Meu Painel
+            </Link>
+          )}
+        </nav>
+        {status === "loading" ? (
+          <>
+            <h1>Opção 1</h1>
+          </>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            {" "}
+            Olá, {session?.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.loginButton}
+            onClick={() => signIn("google")}
+          >
+            {" "}
+            Acessar{" "}
+          </button>
+        )}
+      </section>
+    </header>
+  );
 }
--- tarefas/tarefas/src/pages/dashboard/index.tsx
+++ tarefas/tarefas/src/pages/dashboard/index.tsx
@@ -1,15 +1,28 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
+import { getSession } from 'next-auth/react'
 
 export default function Dashboad (){
     return(
         <div className={styles.dashboard}>
             <Head>
-                <h1>Painel de Tarefas</h1>
+                <title>Painel de Tarefas</title>
             </Head>
             
             <h1>Página Painel</h1>
 
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req})=>{
+
+    const session = await getSession({ req });
+
+    console.log(session)
+
+        return{
+            props:{},
+        }
 }
