77e8fe58da0703304ad03ded50ff3a19176c9ee6
Victor Adauto <victoradauto@yahoo.com.br>
2023-04-28 15:53:52 -0300

    estilização da página detahes de tarefas

--- tarefas/tarefas/src/pages/task/[id].tsx
+++ tarefas/tarefas/src/pages/task/[id].tsx
@@ -1,74 +1,90 @@
-import Head from 'next/head'
-import styles from './styles.module.css'
-import { GetServerSideProps } from 'next'
+import Head from "next/head";
+import styles from "./styles.module.css";
+import { GetServerSideProps } from "next";
 
-import { db } from '../../services/firebaseConnection'
-import { 
-    doc,
-    collection,
-    query,
-    where,
-    getDoc
-} from 'firebase/firestore'
+import { db } from "../../services/firebaseConnection";
+import { doc, collection, query, where, getDoc } from "firebase/firestore";
 
+import { Textearea } from '../../components/textarea'
 
-export default function Task(){
-    return(
-        <div className={styles.container}>
-            <Head>
-                <title>
-                    Detalhes da tarefa
-                </title>
-            </Head>
-            <main className={styles.main}>
-                <h1>Tarefa</h1>
-            </main>
-        </div>
-    )
+interface TaskProps {
+  item: {
+    tarefa: string;
+    public: boolean;
+    created: string;
+    user: string;
+    taskId: string;
+  };
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params})=>{
+export default function Task({ item }: TaskProps) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefa - Detalhes da tarefa</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item.tarefa}</p>
+        </article>
+      </main>
 
-    const id = params?.id as string
+      <section className={styles.commentsContainer}>
 
-    const docRef = doc(db, "tarefas", id)
+        <h2>Fazer comentário</h2>
 
-    const snapShot = await getDoc(docRef)
-  
+        <form>
+            <Textearea 
+            placeholder="Digite seu comentário..."/>
+            <button className={styles.button}>Enviar comentário</button>
+        </form>
 
-    if( snapShot.data() === undefined){
-        return{
-            redirect:{
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-
-    if (!snapShot.data()?.public ){
-        return{
-            redirect:{
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-
-    const milisecond = snapShot.data()?.created?.seconds * 1000
-
-    const task = {
-        tarefa: snapShot.data()?.tarefa,
-        public: snapShot.data()?.public,
-        created: new Date(milisecond).toLocaleDateString(),
-        user: snapShot.data()?.user,
-        taskId: id,
-
-    }
-
-    console.log(task)
-
-    return{
-        props:{}
-    }
-
+      </section>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id as string;
+
+  const docRef = doc(db, "tarefas", id);
+
+  const snapShot = await getDoc(docRef);
+
+  if (snapShot.data() === undefined) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!snapShot.data()?.public) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const milisecond = snapShot.data()?.created?.seconds * 1000;
+
+  const task = {
+    tarefa: snapShot.data()?.tarefa,
+    public: snapShot.data()?.public,
+    created: new Date(milisecond).toLocaleDateString(),
+    user: snapShot.data()?.user,
+    taskId: id,
+  };
+
+  console.log(task);
+
+  return {
+    props: {
+      item: task,
+    },
+  };
+};
