f0bc55098db8267f6f61c11e332b69491ad8913b
victoradauto1 <victoradauto@yahoo.com.br>
2023-07-07 16:53:08 -0300

    primeiras configurações da página about

---
+++ proje_sujeito/sujeito/src/pages/sobre/index.tsx
@@ -0,0 +1,12 @@
+import { GetStaticProps } from "next"
+
+export default function Sobre () {
+
+    return(
+        <div>
+            <h1>Página Sobre</h1>
+        </div>
+    )
+} 
+
+export const getStaticProps: GetStaticProps
