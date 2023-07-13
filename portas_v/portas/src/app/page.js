9ded7de0685f0eca7886f53b235e3f9bced6a059
victoradauto1 <victoradauto@yahoo.com.br>
2023-07-12 22:29:13 -0300

    configuração incial de portas

--- portas_v/portas/src/app/page.js
+++ portas_v/portas/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from '../styles/page.module.css'
 import Presente from '@/components/presente/Presente'
+import Porta from '@/components/porta/Porta'
 
 export default function Home() {
   return (
     <>
-      <Presente/>
+      <Porta/>
     </>
   )
 }
---
+++ portas_v/portas/src/components/porta/Porta.jsx
@@ -0,0 +1,10 @@
+import styles from './porta.module.css'
+
+export default function Porta () {
+
+    return(
+        <div className={styles.porta}>
+          <div className={styles.numero}>3</div>
+        </div>
+    )
+}
