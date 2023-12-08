a4f2adb038dade71a0f393b4d9e451aa2ff0e09e
victoradauto1 <victoradauto@yahoo.com.br>
2023-12-08 17:06:18 -0300

    criação do projeto renderização

--- portas_final/portas/src/pages/jogo/[portas1]/[temPresente].tsx
+++ portas_final/portas/src/pages/jogo/[portas1]/[temPresente].tsx
@@ -2,46 +2,67 @@ import styles from "../../../styles/jogo.module.css";
 import { useState, useEffect } from "react";
 import Porta from "../../../components/porta/[temPresente]";
 import { atualizarPortas, criarPortas } from "@/functions/portas";
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import PortaModel from "@/model/porta"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import PortaModel from "@/model/porta";
 
 export default function jogo() {
+  const router = useRouter();
 
-  const router = useRouter()
-
+  const [valido, setValido] = useState(false);
   const [portas, setPortas] = useState<PortaModel[]>([]);
 
   // console.log(router?.query.portas, router?.query.temPresente)
 
- useEffect(() => {
-  const portas = router.query.portas1? +router.query.portas1 : 4
-  const temPresente = router.query.temPresente? +router.query.temPresente: 0
-  setPortas(criarPortas(portas, temPresente))
-  console.log(router.query)
-}, [router?.query]);
-  
+  useEffect(() => {
+    const portas = router.query.portas1 ? +router.query.portas1 : 0;
+    const temPresente = router.query.temPresente
+      ? +router.query.temPresente
+      : 0;
+    const qtdPortasValida = portas >= 3 && portas < 30;
+    const temPresenteValido = temPresente <= portas && temPresente >= 1;
+    setValido(qtdPortasValida && temPresenteValido);
+  }, [portas]);
+
+  useEffect(() => {
+    const portas = router.query.portas1 ? +router.query.portas1 : 0;
+    const temPresente = router.query.temPresente
+      ? +router.query.temPresente
+      : 0;
+    setPortas(criarPortas(portas, temPresente));
+    console.log(router.query);
+  }, [router?.query]);
+
   function renderizarPortas() {
-    return portas.map((porta:PortaModel) => {
-      return (
-        <Porta
-          key={porta.numero}
-          value={porta}
-          onChange={(novaPorta) =>
-            setPortas(atualizarPortas(portas, novaPorta))
-          }
-        />
-      );
-    });
+    return valido ? (
+      portas.map((porta: PortaModel) => {
+        return (
+          <Porta
+            key={porta.numero}
+            value={porta}
+            onChange={(novaPorta) =>
+              setPortas(atualizarPortas(portas, novaPorta))
+            }
+          />
+        );
+      })
+    ) : (
+      <div className={styles.aviso}>
+        <h1> Valor inserido é inválido!</h1>
+        <p>
+          O número de portas escolhido deve estar entre 3 e 30, e a porta com
+          presente deve estar compreendida entre 1 e o número de portas
+          escolhidos
+        </p>
+      </div>
+    );
   }
 
   return (
     <div className={styles.jogo}>
-      <div className={styles.portas}>
-        {renderizarPortas()}
-      </div>
+      <div className={styles.portas}>{renderizarPortas()}</div>
       <div className={styles.botoes}>
-        <Link href='/'>
+        <Link href="/">
           <button>Reiniciar o Jogo</button>
         </Link>
       </div>
---
+++ renderizacao/app/profile/page.js
@@ -0,0 +1,8 @@
+export default function Profile(){
+
+    return (
+        <div>
+            <h1>Página funcionando</h1>
+        </div>
+    )
+}
