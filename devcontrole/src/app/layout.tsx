1a6f9b188880f72bb9cfabe60890a1a2cdeb253c
victoradauto1 <victoradauto@yahoo.com.br>
2024-02-08 17:07:33 -0300

    criação do projeto devcontrole, criação e estilizaçao do header

---
+++ devcontrole/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dev Controle - Seu sistema de gerenciamento.",
+  description: "Gerencie seu clientes e serviços de forma eficiente e segura",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header/>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ devcontrole/src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FiLogOut, FiUser } from "react-icons/fi";
+
+export function Header() {
+  return (
+    <header className="w-full  flex items-center px-2 py-4 bg-white h-20 shadow-sm">
+      <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+        <Link href="/">
+          <h1 className="font-bold text-xl pl-1 hover:tracking-widest duration-300 transition-all">
+            <span className="text-blue-500">DEV</span>Controle
+          </h1>
+        </Link>
+        <div className="flex items-baseline">
+          <Link href="/dashboard">
+            <FiUser size={24} color="#4b5563" />
+          </Link>
+          <button>
+            <FiLogOut size={24} color="#4b5563" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
